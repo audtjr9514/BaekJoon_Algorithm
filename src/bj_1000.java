11732695ba6b6a4e361372d305c91f0c7be3f54e
myeongs14 <audtjr9514@gmail.com>
2019-01-31 16:00:42 +0900

    19.01.31

---
+++ src/bj_1000.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class bj_1000 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        int x = scan.nextInt();
+        int y = scan.nextInt();
+
+        System.out.println(x+y);
+    }
+}
