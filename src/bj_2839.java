77da89bf795dc2df9089ed4dce0b829dcb35ab84
myeongs14 <audtjr9514@gmail.com>
2019-03-20 21:07:43 +0900

    19.03.20

--- src/bj_2839.java
+++ src/bj_2839.java
@@ -1,18 +1,22 @@
 import java.util.Scanner;
 
 public class bj_2839 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
         int x = scan.nextInt();
-        if((x%5)%3 != 0) {
-            System.out.println(-1);
-        }
-        else if (x%5 != 0) {
-            System.out.println((x/5)+(x%5)/3);
-        }
-        else {
-            System.out.println((x/5));
+        if (x == 1 || x == 2 || x == 4 || x == 7) {
+            System.out.print(-1);
+        } else if (x % 5 == 0) {
+            System.out.print((x / 5));
+        } else {
+            if ((x % 5) % 3 != 0) {
+                System.out.print(x / 3);
+            } else if ((x / 5) + ((x % 5) / 3) >= (x / 3)) {
+                System.out.print(x / 3);
+            } else {
+                System.out.print((x / 5) + ((x % 5) / 3));
+            }
         }
     }
 }
