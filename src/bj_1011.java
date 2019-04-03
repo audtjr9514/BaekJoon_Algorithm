5aece910b982e6486df3f860c3b7747f4fdee2dc
myeongs14 <audtjr9514@gmail.com>
2019-04-04 00:28:41 +0900

    19.04.04

--- src/bj_1011.java
+++ src/bj_1011.java
@@ -1,3 +1,13 @@
+/*
+ 양 쪽 거리에서 1씩 증가를 시켜본다.
+ 예를 들어, 거리가 16일때 총 7회 1 + 2 + 3 + 4 + 3 + 2 +1
+ 가운데 가장 큰수를 기준으로 양 쪽이 대칭을 이룰 때 최대 이동횟수를 찍는다.
+ 거리 = y-x 이므로 1, 1, 2, 2, 3, 3, 4 ~~~
+ 각 이동이 될때마다 카운트를 센다.
+ 각 이동이 진행될때마다 거리에서 각 단계를 빼준다.
+ 거리가 0이 되거나 0보다 작은 경우의 카운트가 이동횟수가 된다.
+ */
+
 import java.util.Scanner;
 
 public class bj_1011 {
@@ -7,18 +17,26 @@ public class bj_1011 {
 
         for (int i = 0; i < testcase.length; i++) {
             int x = scan.nextInt();
-            int y = scan.nextInt() - 1;
-            int num = 0, move = 1;
+            int y = scan.nextInt();
+            int distance = y - x, moving = 0, count = 0;
             while (true) {
-                num += move;
-                move++;
-                if (x + num >= y)
+                moving++;
+                distance -= moving;
+                count++;
+                if (distance <= 0)
                     break;
+                else {
+                    distance -= moving;
+                    count++;
+                    if (distance <= 0)
+                        break;
+                }
             }
-            testcase[i] = move;
+            testcase[i] = count;
         }
-        for(int cases : testcase)
-            System.out.println(cases);
 
+        for (int test : testcase)
+            System.out.println(test);
+        scan.close();
     }
 }
--- src/bj_2292.java
+++ src/bj_2292.java
@@ -1,4 +1,12 @@
-import java.util.Scanner;
+/*
+입력숫자   이동
+1         | 1 |                 1
+2 ~ 7     | 2 |     6*0 + 2     6*1 + 1
+8 ~ 19    | 3 |     6*1 + 2     6*(1+2) + 1
+20 ~ 37   | 4 |     6*3 + 2     6*(1+2+3) + 1
+38 ~ 61   | 5 |     6*6 + 2     6*(1+2+3+4) +1
+ */
+ import java.util.Scanner;
 
 public class bj_2292 {
     public static void main(String[] args) {
@@ -12,11 +20,4 @@ public class bj_2292 {
         System.out.println(count);
     }
 }
-/*
-1       /1/             1
-2 ~ 7   /2/   6*0 + 2   6*1 + 1
-8 ~ 19  /3/   6*1 + 2   6*(1+2) + 1
-20 ~ 37 /4/   6*3 + 2   6*(1+2+3) + 1
-38 ~ 61 /5/   6*6 + 2   6*(1+2+3+4) +1
- */
 
