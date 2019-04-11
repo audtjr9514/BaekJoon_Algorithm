18f4260596a23d0b74ede9874d08219655f44b84
myeongs14 <audtjr9514@gmail.com>
2019-04-12 00:11:57 +0900

    19.04.12

---
+++ src/bj_6064.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class bj_6064 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[] test = new int[scan.nextInt()];
+        for (int i = 0; i < test.length; i++) {
+            int m = scan.nextInt();
+            int n = scan.nextInt();
+            int x = scan.nextInt();
+            int y = scan.nextInt();
+
+        }
+        for(int n : test)
+            System.out.println(n);
+        scan.close();
+
+    }
+}
