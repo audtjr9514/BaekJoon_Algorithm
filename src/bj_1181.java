9eefe60c8641e3fc4ccdd616cbbcf20114464a8a
myeongs14 <audtjr9514@gmail.com>
2019-04-14 00:06:45 +0900

    19.04.14

---
+++ src/bj_1181.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class bj_1181 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] strs = new String[scan.nextInt()];
+        for (int i = 0; i < strs.length; i++)
+            strs[i] = scan.next();
+        for (int i = 0; i < strs.length; i++){
+
+        }
+    }
+}
