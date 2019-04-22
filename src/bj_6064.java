e429cfed6aad255aa67f37f8274cb5c8ab0a2576
myeongs14 <audtjr9514@gmail.com>
2019-04-22 18:18:23 +0900

    19.04.22

--- src/bj_6064.java
+++ src/bj_6064.java
@@ -9,11 +9,27 @@ public class bj_6064 {
             int n = scan.nextInt();
             int x = scan.nextInt();
             int y = scan.nextInt();
+            int x_c = 1, y_c = 1, check = 1;
+            test[i] = 1;
+            while (true) {
+                if (x == x_c) check = m;
+                else if (y == y_c) check = n;
 
+                if ((x_c + check) % m == 0) x_c = m;
+                else x_c = (x_c + check) % m;
+                if ((y_c + check) % n == 0) y_c = n;
+                else y_c = (y_c + check) % n;
+                test[i] += check;
+
+                if (x == x_c && y == y_c) break;
+                if (test[i] > m * n) {
+                    test[i] = -1;
+                    break;
+                }
+            }
         }
-        for(int n : test)
+        for (int n : test)
             System.out.println(n);
         scan.close();
-
     }
 }
