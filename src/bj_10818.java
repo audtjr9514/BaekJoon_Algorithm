7544ee4581ba8ee7036ddb10de0cbe68e9281a1c
audtjr9514 <audtjr9514@gmail.com>
2020-01-30 16:03:43 +0900

    20.01.30

--- src/bj_10818.java
+++ src/bj_10818.java
@@ -5,8 +5,8 @@ public class bj_10818 {
         Scanner scan = new Scanner(System.in);
         int[] arr = new int[scan.nextInt()];
 
-        for (int i = 0; i < arr.length; i++){
-
-        }
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = scan.nextInt();
+        System.out.println(arr);
     }
 }
