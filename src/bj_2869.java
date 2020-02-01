21072474bc7341bdc0b14127b1649dfca435ad14
audtjr9514 <audtjr9514@gmail.com>
2020-02-01 20:07:49 +0900

    20.02.01

--- src/bj_2869.java
+++ src/bj_2869.java
@@ -7,10 +7,13 @@ public class bj_2869 {
         int up = scan.nextInt();
         int down = scan.nextInt();
         int height = scan.nextInt();
-        int day = height / (up - down);
-        if (height - day * (up - down) == 0)
-            System.out.println(day-1);
-        else
-            System.out.println(day);
+
+        int day = (height - down) / (up - down);
+        // 정상을 도착할 때 내려가는 일은 없으므로 정상에서 Down만큼 빼준다.
+        if ((height - down) % (up - down) > 0)
+            day++;
+        // 하지만 하루치 이하의 높이가 남았을수도 있으므로 만약 나머지가 있을 경우 하루를 추가해준다.
+        System.out.println(day);
+        scan.close();
     }
 }
