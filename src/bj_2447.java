049618c81613123cc2911dd963f210c3022a5b3c
audtjr9514 <audtjr9514@gmail.com>
2020-02-10 22:00:20 +0900

    20.02.10

--- src/bj_2447.java
+++ src/bj_2447.java
@@ -1,18 +1,40 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class bj_2447 {
-    public static void star(int n) {
+    public static char[][] stars;
 
+    public static void stars(int x, int y, int n) {
+        if (n == 1) {
+            stars[x][y] = '*';
+            return;
+        }
+        int m = n / 3;
+
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++) {
+                if (i == 1 && j == 1) {
+                    continue;
+                }
+                stars(x + m * i, y + m * j, m);
+            }
     }
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        for (int i = 0; i < 3; i++)
-            for (int j = 0; j < 3; j++)
-                star(n);
-        scan.close();
 
+        int num = scan.nextInt();
+        stars = new char[num][num];
+
+        for (int i = 0; i < num; i++)
+            Arrays.fill(stars[i], ' ');
+
+        stars(0, 0, num);
+
+        for (int i = 0; i < num; i++)
+            System.out.println(stars[i]);
+        scan.close();
     }
 }
+
+
 
--- src/bj_2798.java
+++ src/bj_2798.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class bj_2798 {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
     }
 }
