2c5674f81349a1355c2c7926ad9f17f7294b194e
audtjr9514 <audtjr9514@gmail.com>
2020-02-11 00:06:23 +0900

    20.02.11

--- src/bj_2798.java
+++ src/bj_2798.java
@@ -1,31 +1,28 @@
 import java.util.Scanner;
-import java.lang.Math;
 
 public class bj_2798 {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        int num = scan.nextInt();
-        int max = scan.nextInt();
+        int num = scan.nextInt(); // 주어질 카드의 갯수
+        int max = scan.nextInt(); // 기준 값
         int[] card = new int[num];
-        int[] last = new int[3];
 
         for (int i = 0; i < num; i++)
             card[i] = scan.nextInt();
 
-        int sum = 0;
-        outline:
+
+        int temp = 0;
         for (int i = 0; i < num - 2; i++)
             for (int j = i + 1; j < num - 1; j++)
                 for (int z = j + 1; z < num; z++) {
-                    sum += (card[i] + card[j] + card[z]);
+                    int sum = card[i] + card[j] + card[z];
                     if (sum <= max) {
-                        last[0] = card[i];
-                        last[1] = card[j];
-                        last[2] = card[z];
+                        if (temp <= sum)
+                            temp = sum;
                     }
-                    sum = 0;
                 }
-        System.out.print(last[0] + last[1] + last[2]);
+        System.out.println(temp);
+        scan.close();
     }
 }
