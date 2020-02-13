de373feb906fdae014f7dc49a62d039b940d731a
audtjr9514 <audtjr9514@gmail.com>
2020-02-13 20:19:19 +0900

    20.04.16

---
+++ src/bj_1247.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class bj_1247 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("문제의 정답");
+        br.close();
+        bw.close();
+    }
+}
