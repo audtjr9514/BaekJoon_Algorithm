4aff67726d9fa6cfe4837b7007f345fa14f7c045
audtjr9514 <audtjr9514@gmail.com>
2020-03-04 03:24:50 +0900

    20.03.04

--- src/bj_14888.java
+++ src/bj_14888.java
@@ -8,39 +8,37 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class bj_14888 {
-    static int[] numbers;
+    static int[] numbers; // 초기 수열
     static int[] operation = new int[4]; // 사칙 연산
-    static ArrayList<Integer> end_result = new ArrayList<Integer>();
+    static ArrayList<Integer> end_result = new ArrayList<>();
 
-    static void oper(int level, int result) {
-        if (level == numbers.length - 1) {
+    static void oper(int level, int result) throws IOException {
+        if (level == numbers.length - 1) {// 모든 단계를 거치면 리스트에 추가
             end_result.add(result);
             return;
         } else {
             for (int i = 0; i < 4; i++) {
                 if (operation[i] > 0) {
                     operation[i]--;
-                    result = check(i, level);
-                    oper(level + 1, result);
+                    int next = chk_result(i, level + 1, result);
+                    // next는 현재 레벨의 값과, 그 다음의 레벨 값을 계산해서 넣는 임시 변수
+                    oper(level + 1, next);
                     operation[i]++;
                 }
             }
         }
-
     }
 
-    static int check(int i, int level) {
-        switch (i) {
-            case 0:
-                return numbers[level] + numbers[level + 1];
-            case 1:
-                return numbers[level] - numbers[level + 1];
-            case 2:
-                return numbers[level] * numbers[level + 1];
-            case 3:
-                return numbers[level] / numbers[level + 1];
-        }
-        return 0;
+    static int chk_result(int i, int level, int result) {
+        // 1:+ , 2:-, 3:*, 4:/
+        if (i == 0)
+            return result + numbers[level];
+        else if (i == 1)
+            return result - numbers[level];
+        else if (i == 2)
+            return result * numbers[level];
+        else
+            return result / numbers[level];
     }
 
     public static void main(String[] args) throws IOException {
@@ -58,18 +56,14 @@ public class bj_14888 {
         for (int i = 0; i < operation.length; i++)
             operation[i] = Integer.parseInt(st.nextToken());
 
-        int result = 0;
-        oper(0, result);
+
+        int result = numbers[0];// 초기 계산을 위한 시작값
+        oper(0, result); //단계는 0에서 시작, 수열의 첫번째 값을 시작으로 넣는다.
         Collections.sort(end_result);
 
-        for (int x : end_result)
-            bw.write(String.valueOf(x) + ' ');
-
-        bw.newLine();
-        bw.write(String.valueOf(end_result.get(end_result.size() - 1)) + '\n' + String.valueOf(end_result.get(0)));
+        bw.write(String.valueOf(end_result.get(end_result.size() - 1)) + '\n' + end_result.get(0));
 
         br.close();
         bw.close();
-
     }
 }
--- src/testcode.java
+++ src/testcode.java
@@ -1,15 +1,115 @@
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class testcode {
+    public static int[][] arr = new int[9][9];
+    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String n = Integer.toString(br.read());
-        String m = Integer.toString(br.read());
 
-        bw.write(n + " " + m);
 
-        br.close();
+        for (int i = 0; i < 9; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 9; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+
+            }
+        }
+
+        backtracking(0, 0);
+
+
+    }
+
+    public static void backtracking(int row, int col) throws IOException {
+        if (col == 9) {
+            col = 0;
+            row++;
+        }
+
+        if (row == 9) {
+            print();
+            System.exit(0); //스도쿠 판을 채우는 방법이 여럿인 경우는 그 중 하나만을 출력하기 위해 시스템 종료
+        }
+
+
+        if (arr[row][col] == 0) { //0인 부분의 숫자를 탐색
+            for (int i = 1; i < 10; i++) {
+                if (isPossible(row, col, i)) { //0인 부분의 값을 임의로 i를 넣어놓고 그 값이 가능하다고 판단되면
+                    arr[row][col] = i;
+                    backtracking(row, col + 1);
+                    arr[row][col] = 0;
+                }
+            }
+        } else backtracking(row, col + 1);
+
+    }
+
+    public static boolean isPossible(int row, int col, int value) {
+//        boolean[] visited = new boolean[10]; //1~9까지 숫자가 사용됐는지 체크
+
+        for (int i = 0; i < 9; i++) {
+            if (arr[row][i] == value) return false; //해당 위치의 행을 전체 탐색, 인자로 넘어온 value가 이미 다른 열에 있다면 false 리턴
+            if (arr[i][col] == value) return false; //해당 위치의 열을 전체 탐색, 인자로 넘어온 value가 이미 다른 열에 있다면 false 리턴
+        }
+//        if(!checkSquare(row, col, value)) return false;
+
+        row = (row / 3) * 3;
+        col = (col / 3) * 3;
+
+        for (int i = row; i < row + 3; i++) {
+            for (int j = col; j < col + 3; j++) {
+                if (arr[i][j] == value) return false;
+            }
+        }
+
+//        checkGaro(row, visited);
+//        checkSero(col, visited);
+//        checkSquare(row, col, visited);
+
+//        for (i = 1; i < visited.length; i++)
+//            if (visited[i] != true) ;
+
+        return true;
+    }
+
+//    public static boolean[] checkGaro(int row, boolean[] visited) {
+//        for (int i = 0; i < 9; i++) {
+//            visited[Integer.parseInt(arr[row][i])] = true;
+//        }
+//        return visited;
+//    }
+//
+//    public static boolean[] checkSero(int col, boolean[] visited) {
+//        for (int i = 0; i < 9; i++) {
+//            visited[Integer.parseInt(arr[i][col])] = true;
+//        }
+//
+//        return visited;
+//    }
+//
+//    public static boolean checkSquare(int row, int col, int value) {
+//        row = (row / 3) * 3;
+//        col = (col / 3) * 3;
+//
+//        for (int i = row; i < row + 3; i++) {
+//            for (int j = col; j < col + 3; j++) {
+//                if (arr[i][j].equals(value)) return false;
+//            }
+//        }
+//
+//        return true;
+//    }
+
+    public static void print() throws IOException {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                bw.write(arr[i][j] + " ");
+            }
+            bw.newLine();
+        }
         bw.close();
     }
+
 }
