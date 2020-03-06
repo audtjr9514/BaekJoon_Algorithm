a0580d10967a17b7a957a5d5278550f340519fc2
audtjr9514 <audtjr9514@gmail.com>
2020-03-07 01:25:02 +0900

    20.03.07

--- src/bj_1149.java
+++ src/bj_1149.java
@@ -3,15 +3,24 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class bj_1149 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int testcase = Integer.parseInt(br.readLine());
-        for (int i = 1; i > testcase; i++) {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int
 
-        }
+    static long RGB(int n) throws IOException {
+        int r = Integer.parseInt(br.readLine());
+        int g = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        bw.write(String.valueOf(RGB(N)));
 
         br.close();
         bw.close();
