7f25234eb78acd8d5d09ea0bd9fa09dc59ab873f
audtjr9514 <audtjr9514@gmail.com>
2020-03-10 02:31:48 +0900

    20.03.10

--- src/bj_2579.java
+++ src/bj_2579.java
@@ -9,17 +9,21 @@ public class bj_2579 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
-        long[] stairs = new long[N+1];
-        long[] sum = new long[N+1];
+        long[] stairs = new long[N];
+        long[] sum = new long[N];
 
-        for (int i = 1; i <= N; i++) {
+        for (int i = 0; i < N; i++) {
             stairs[i] = Integer.parseInt(br.readLine());
         }
-        sum[1] = stairs[1];
-        sum[2] = stairs[1] + stairs[2];
-        for (int i = 3; i <= N; i++)
-            sum[i] = Math.max(sum[i - 2] + stairs[i], sum[i - 3] + stairs[i - 1] + stairs[i]);
-        bw.write(String.valueOf(sum[N]));
+        if (N >= 1) sum[0] = stairs[0];
+        if (N >= 2) sum[1] = (stairs[0] + stairs[1]) > stairs[1] ? stairs[0] + stairs[1] : stairs[1];
+        if (N >= 3) {
+            sum[2] = (stairs[0] + stairs[2]) > (stairs[1] + stairs[2]) ? (stairs[0] + stairs[2]) : (stairs[1] + stairs[2]);
+            for (int i = 3; i < N; i++)
+                sum[i] = Math.max(sum[i - 2] + stairs[i], sum[i - 3] + stairs[i - 1] + stairs[i]);
+        }
+
+        bw.write(String.valueOf(sum[N - 1]));
         br.close();
         bw.close();
     }
