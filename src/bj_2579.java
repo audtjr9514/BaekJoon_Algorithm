1c3d0e090ec5ab3f989646d083c81a64528b778a
audtjr9514 <audtjr9514@gmail.com>
2020-03-09 23:37:12 +0900

    20.03.09

--- src/bj_2579.java
+++ src/bj_2579.java
@@ -5,19 +5,22 @@ import java.io.OutputStreamWriter;
 import java.io.IOException;
 
 public class bj_2579 {
-    static int[] stairs;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
-        stairs = new int[N];
-        for (int i = 0; i < N; i++) {
+        long[] stairs = new long[N+1];
+        long[] sum = new long[N+1];
+
+        for (int i = 1; i <= N; i++) {
             stairs[i] = Integer.parseInt(br.readLine());
         }
-
+        sum[1] = stairs[1];
+        sum[2] = stairs[1] + stairs[2];
+        for (int i = 3; i <= N; i++)
+            sum[i] = Math.max(sum[i - 2] + stairs[i], sum[i - 3] + stairs[i - 1] + stairs[i]);
+        bw.write(String.valueOf(sum[N]));
         br.close();
         bw.close();
-
     }
 }
