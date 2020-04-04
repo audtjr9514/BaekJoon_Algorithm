8c19eacdd26c1db8645d39abad0fdd52757a364e
audtjr9514 <audtjr9514@gmail.com>
2020-04-05 03:23:38 +0900

    20.04.05

--- src/bj_9251.java
+++ src/bj_9251.java
@@ -10,14 +10,17 @@ public class bj_9251 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         char[] str1 = br.readLine().toCharArray();
         char[] str2 = br.readLine().toCharArray();
-        int[][] dp = new int[str1.length][str2.length];
+        int[][] dp = new int[str1.length+1][str2.length+1];
 
         for (int i = 0; i < str1.length; i++) {
-
+            for (int j = 0; j < str2.length; j++) {
+                if (str1[i] != str2[j])
+                    dp[j + 1][i + 1] = Math.max(dp[j + 1][i], dp[j][i + 1]);
+                else
+                    dp[j + 1][i + 1] = dp[j][i] + 1;
+           }
         }
-
-        bw.write(dp[str1.length - 1][str2.length - 1]);
-
+        bw.write(String.valueOf(dp[str1.length][str2.length]));
 
         br.close();
         bw.close();
