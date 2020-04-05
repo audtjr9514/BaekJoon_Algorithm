1e47579540f35536de413c680b6126720bbbd0b5
audtjr9514 <audtjr9514@gmail.com>
2020-04-06 02:12:51 +0900

    20.04.06

--- src/bj_9251.java
+++ src/bj_9251.java
@@ -8,19 +8,19 @@ public class bj_9251 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        char[] str1 = br.readLine().toCharArray();
-        char[] str2 = br.readLine().toCharArray();
-        int[][] dp = new int[str1.length+1][str2.length+1];
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+        int[][] dp = new int[str2.length() + 1][str1.length() + 1];
 
-        for (int i = 0; i < str1.length; i++) {
-            for (int j = 0; j < str2.length; j++) {
-                if (str1[i] != str2[j])
-                    dp[j + 1][i + 1] = Math.max(dp[j + 1][i], dp[j][i + 1]);
+        for (int i = 1; i <= str2.length(); i++) {
+            for (int j = 1; j <= str1.length(); j++) {
+                if (str2.charAt(i-1) != str1.charAt(j-1))
+                    dp[i][j] = Math.max(dp[i][j-1],dp[i-1][j]);
                 else
-                    dp[j + 1][i + 1] = dp[j][i] + 1;
-           }
+                    dp[i][j] = dp[i-1][j-1]+1;
+            }
         }
-        bw.write(String.valueOf(dp[str1.length][str2.length]));
+        bw.write(String.valueOf(dp[str1.length()][str2.length()]));
 
         br.close();
         bw.close();
