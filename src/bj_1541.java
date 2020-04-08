c3b86b88bae7f45d2cb12a066c286b1bc1314986
audtjr9514 <audtjr9514@gmail.com>
2020-04-08 19:21:14 +0900

    20.04.08

---
+++ src/bj_1541.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class bj_1541 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str = br.readLine();
+
+        br.close();
+        bw.close();
+    }
+}
