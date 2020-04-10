52d3fea93d88ba3cc1f3cfb4950d8ff5c49abf3b
audtjr9514 <audtjr9514@gmail.com>
2020-04-10 10:05:27 +0900

    20.04.10

--- src/bj_1541.java
+++ src/bj_1541.java
@@ -9,6 +9,22 @@ public class bj_1541 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String str = br.readLine();
+        String[] strs = str.split("-");
+        int temp = 0;
+        int sum = 0;
+        for (int i = 0; i < strs.length; i++) {
+            if (i == 0) {
+                String[] strs_plus = strs[i].split("\\+");
+                for (String s : strs_plus)
+                    sum += Integer.parseInt(s);
+            }
+            else{
+                String[] strs_plus = strs[i].split("\\+");
+                for (String s : strs_plus)
+                    sum -= Integer.parseInt(s);
+            }
+        }
+        bw.write(String.valueOf(sum));
 
         br.close();
         bw.close();
