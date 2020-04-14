e1de679d3daff03ef980e7ddf637a1d968a00c86
audtjr9514 <audtjr9514@gmail.com>
2020-04-14 20:08:01 +0900

    20.04.14

--- src/bj_12865.java
+++ src/bj_12865.java
@@ -12,14 +12,26 @@ public class bj_12865 {
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
-        int[][] bag = new int[N][2];
         int Weight = Integer.parseInt(st.nextToken());
-        for(int i =0; i < N;i++){
+        int[][] dp = new int[N + 1][Weight + 1];
+
+        int[] W = new int[N + 1];
+        int[] V = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            bag[i][0] = Integer.parseInt(st.nextToken());
-            bag[i][1] = Integer.parseInt(st.nextToken());
+            W[i] = Integer.parseInt(st.nextToken());
+            V[i] = Integer.parseInt(st.nextToken());
         }
 
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= Weight; j++) {
+                dp[i][j] = dp[i - 1][j]; // 기본적으로 이전 아이템의 가치를 저장한다.
+                if (j - W[i] >= 0) { // 무게에서 자신의 무게를 뺐을 때 남는 무게가 존재하면,
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - W[i]] + V[i]); // 이전 아이템에서 구한 가치와 남은 무게의 가치 + 자신의 가치 중 큰 값을 취한다.
+                }
+            }
+        }
+        bw.write(String.valueOf(dp[N][Weight]));
 
         br.close();
         bw.close();
