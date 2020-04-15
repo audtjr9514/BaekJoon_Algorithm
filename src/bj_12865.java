aae3965c42fa8e79b9c36db28ecfc3ffd0fdc654
audtjr9514 <audtjr9514@gmail.com>
2020-04-15 23:29:53 +0900

    20.04.14

--- src/bj_12865.java
+++ src/bj_12865.java
@@ -25,9 +25,9 @@ public class bj_12865 {
 
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= Weight; j++) {
-                dp[i][j] = dp[i - 1][j]; // 기본적으로 이전 아이템의 가치를 저장한다.
-                if (j - W[i] >= 0) { // 무게에서 자신의 무게를 뺐을 때 남는 무게가 존재하면,
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - W[i]] + V[i]); // 이전 아이템에서 구한 가치와 남은 무게의 가치 + 자신의 가치 중 큰 값을 취한다.
+                dp[i][j] = dp[i - 1][j];
+                if (j - W[i] >= 0) {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - W[i]] + V[i]);
                 }
             }
         }
