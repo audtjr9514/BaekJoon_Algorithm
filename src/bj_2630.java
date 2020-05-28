45736af20e6b65dccc4f138f3ce7c689e30c83fa
audtjr9514 <audtjr9514@gmail.com>
2020-05-28 19:08:09 +0900

    20.05.28

---
+++ src/bj_2630.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class bj_2630 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int  n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][n];
+        StringTokenizer st;
+
+        br.close();
+        bw.close();
+    }
+}
