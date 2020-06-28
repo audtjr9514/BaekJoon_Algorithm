d16d09b802d3d41adf4dbc40ea94e84f040f1b9a
audtjr9514 <audtjr9514@gmail.com>
2020-06-28 18:05:47 +0900

    20.06.28

---
+++ src/bj_1300.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class bj_1300 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        long k = Long.parseLong(br.readLine());
+
+
+
+
+        br.close();
+        bw.close();
+    }
+}
