2d5fbe207bdba9c8f712e521345d297cd547f372
audtjr9514 <audtjr9514@gmail.com>
2020-09-05 21:24:48 +0900

    20.09.05

--- src/bj_1504.java
+++ src/bj_1504.java
@@ -37,10 +37,10 @@ public class bj_1504 {
 
     static int V;
     static int E;
-    static int v1, v2;
     static List<Node>[] vertexList;
     static int[] distance;
-    static final int INF = Integer.MAX_VALUE;
+    static boolean[] visited;
+    static final int INF = 200_000_000;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -50,6 +50,7 @@ public class bj_1504 {
         E = Integer.parseInt(st.nextToken());
         vertexList = new ArrayList[V + 1];
         distance = new int[V + 1];
+        visited = new boolean[V + 1];
         for (int i = 1; i < V + 1; i++) {
             vertexList[i] = new ArrayList<>();
         }
@@ -59,20 +60,20 @@ public class bj_1504 {
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
             int weight = Integer.parseInt(st.nextToken());
+
             vertexList[start].add(new Node(end, weight));
             vertexList[end].add(new Node(start, weight));
         }
         st = new StringTokenizer(br.readLine());
-        v1 = Integer.parseInt(st.nextToken());
-        v2 = Integer.parseInt(st.nextToken());
+        int v1 = Integer.parseInt(st.nextToken());
+        int v2 = Integer.parseInt(st.nextToken());
         bw.write(String.valueOf(solve(v1, v2)));
 
         br.close();
         bw.close();
     }
 
-    static int dijkstra(int start, int end) {
-        boolean[] visited = new boolean[V + 1]; //각 점들의 방문 여부
+    public static int dijkstra(int start, int end) {
         Arrays.fill(distance, INF);
         Arrays.fill(visited, false);
 
@@ -100,17 +101,17 @@ public class bj_1504 {
         return distance[end];
     }
 
-    private static int solve(int required1, int required2) {
+    public static int solve(int v1, int v2) {
         int result1 = 0;
         int result2 = 0;
 
-        result1 += dijkstra(1, required1);
-        result1 += dijkstra(required1, required2);
-        result1 += dijkstra(required2, V);
+        result1 += dijkstra(1, v1);
+        result1 += dijkstra(v1, v2);
+        result1 += dijkstra(v2, V);
 
-        result2 += dijkstra(1, required2);
-        result2 += dijkstra(required2, required1);
-        result2 += dijkstra(required1, V);
+        result2 += dijkstra(1, v2);
+        result2 += dijkstra(v2, v1);
+        result2 += dijkstra(v1, V);
 
         if (result1 >= INF && result2 >= INF)
             return -1;
